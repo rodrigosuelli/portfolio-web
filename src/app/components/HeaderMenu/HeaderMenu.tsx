c44f80a020bed93942d783cd66333d6aa30e1bfa
rodrigosuelli <rodrigosuelli@gmail.com>
2024-07-27 23:31:46 -0300

    Changes in header component

--- src/app/components/HeaderMenu/HeaderMenu.tsx
+++ src/app/components/HeaderMenu/HeaderMenu.tsx
@@ -4,18 +4,29 @@ import styles from './HeaderMenu.module.scss';
 
 export default function HeaderMenu() {
   return (
-    <header className={classNames(styles.headerMenu)}>
+    <header className={classNames([styles.headerMenu])}>
       <Link className={classNames(styles.logo)} href="/">
         {'<RS />'}
       </Link>
-      <nav className={classNames(styles.headerLinks)}>
-        <Link href="#Sobre">Sobre</Link>
-        <Link href="#Habilidades">Habilidades</Link>
-        <Link href="#Experiência">Experiência</Link>
-        <Link href="#Contato">Contato</Link>
-      </nav>
-      <div className={classNames(styles.headerButtons)}>
-        <button type="button">Hamburguer</button>
+      <button className={classNames(styles.hamburguerBtn)} type="button">
+        Hamburguer
+      </button>
+      <div className={classNames(styles.navContainer)}>
+        <nav className={classNames(styles.headerLinks)}>
+          <Link href="#Sobre">Sobre</Link>
+          <Link href="#Habilidades">Habilidades</Link>
+          <Link href="#Experiência">Experiência</Link>
+          <Link href="#Contato">Contato</Link>
+        </nav>
+        <div className={classNames(styles.divider)}></div>
+        <div className={classNames(styles.headerButtons)}>
+          <button className={classNames(styles.btnBaixarCv)} type="button">
+            Switch Theme
+          </button>
+          <button className={classNames(styles.btnBaixarCv)} type="button">
+            Baixar CV
+          </button>
+        </div>
       </div>
     </header>
   );
