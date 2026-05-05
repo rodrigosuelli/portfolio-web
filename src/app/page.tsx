e2000019d2b023e7687b4cae2d0a6b1aa4942a23
rodrigosuelli <rodrigosuelli@gmail.com>
2026-05-05 19:03:18 -0300

    Update experience duration in Home component description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -35,7 +35,7 @@ export default function Home() {
                 focus on creating (and occasionally designing) exceptional
                 digital experiences that are fast, accessible, visually
                 appealing, and responsive. Even though I have been creating web
-                applications for over 7 years, I still love it as if it was
+                applications for over 5 years, I still love it as if it was
                 something new.
               </Typography>
             </div>
